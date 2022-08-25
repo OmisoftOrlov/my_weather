3a0e98773ffa49daefa47e3aeda5ebf480881f32
Maxim_Orlov <m.orlov@omisoft.net>
2022-08-25 17:13:30 +0300

    created file fo managing string resources

---
+++ lib/data_layer/constants/strings.dart
@@ -0,0 +1,49 @@
+class Strings {
+  
+  Strings._();
+
+  static const String type_1 = "Blowing Or Drifting Snow";
+  static const String type_2 = "Drizzle";
+  static const String type_3 = "Heavy Drizzle";
+  static const String type_4 = "Light Drizzle";
+  static const String type_5 = "Heavy Drizzle/Rain";
+  static const String type_6 = "Light Drizzle/Rain";
+  static const String type_7 = "Dust storm";
+  static const String type_8 = "Fog";
+  static const String type_9 = "Freezing Drizzle/Freezing Rain";
+  static const String type_10 = "Heavy Freezing Drizzle/Freezing Rain";
+  static const String type_11 = "Light Freezing Drizzle/Freezing Rain";
+  static const String type_12 = "Freezing Fog";
+  static const String type_13 = "Heavy Freezing Rain";
+  static const String type_14 = "Light Freezing Rain";
+  static const String type_15 = "Funnel Cloud/Tornado";
+  static const String type_16 = "Hail Showers";
+  static const String type_17 = "Ice";
+  static const String type_18 = "Lightning Without Thunder";
+  static const String type_19 = "Mist";
+  static const String type_20 = "Precipitation In Vicinity";
+  static const String type_21 = "Rain";
+  static const String type_22 = "Heavy Rain And Snow";
+  static const String type_23 = "Light Rain And Snow";
+  static const String type_24 = "Rain Showers";
+  static const String type_25 = "Heavy Rain";
+  static const String type_26 = "Light Rain";
+  static const String type_27 = "Sky Coverage Decreasing";
+  static const String type_28 = "Sky Coverage Increasing";
+  static const String type_29 = "Sky Unchanged";
+  static const String type_30 = "Smoke Or Haze";
+  static const String type_31 = "Snow";
+  static const String type_32 = "Snow And Rain Showers";
+  static const String type_33 = "Snow Showers";
+  static const String type_34 = "Heavy Snow";
+  static const String type_35 = "Light Snow";
+  static const String type_36 = "Squalls";
+  static const String type_37 = "Thunderstorm";
+  static const String type_38 = "Thunderstorm Without Precipitation";
+  static const String type_39 = "Diamond Dust";
+  static const String type_40 = "Hail";
+  static const String type_41 = "Overcast";
+  static const String type_42 = "Partially cloudy";
+  static const String type_43 = "Clear";
+  static const String typeUndefined = "Weather conditions undefined";
+}
--- lib/data_layer/models/condition.dart
+++ lib/data_layer/models/condition.dart
@@ -1,3 +1,5 @@
+import 'package:my_weather/data_layer/constants/strings.dart';
+
 class Condition {
   final String typeId;
   final String description;
@@ -10,179 +12,179 @@ class Condition {
     String imagePath;
     switch (type) {
       case "type_1":
-        description = "Blowing Or Drifting Snow";
+        description = Strings.type_1;
         imagePath = "";
         break;
       case "type_2":
-        description = "Drizzle";
+        description = Strings.type_2;
         imagePath = "";
         break;
       case "type_3":
-        description = "Heavy Drizzle";
+        description = Strings.type_3;
         imagePath = "";
         break;
       case "type_4":
-        description = "Light Drizzle";
+        description = Strings.type_4;
         imagePath = "";
         break;
       case "type_5":
-        description = "Heavy Drizzle/Rain";
+        description = Strings.type_5;
         imagePath = "";
         break;
       case "type_6":
-        description = "Light Drizzle/Rain";
+        description = Strings.type_6;
         imagePath = "";
         break;
       case "type_7":
-        description = "Dust storm";
+        description = Strings.type_7;
         imagePath = "";
         break;
       case "type_8":
-        description = "Fog";
+        description = Strings.type_8;
         imagePath = "";
         break;
       case "type_9":
-        description = "Freezing Drizzle/Freezing Rain";
+        description = Strings.type_9;
         imagePath = "";
         break;
       case "type_10":
-        description = "Heavy Freezing Drizzle/Freezing Rain";
+        description = Strings.type_10;
         imagePath = "";
         break;
       case "type_11":
-        description = "Light Freezing Drizzle/Freezing Rain";
+        description = Strings.type_11;
         imagePath = "";
         break;
       case "type_12":
-        description = "Freezing Fog";
+        description = Strings.type_12;
         imagePath = "";
         break;
       case "type_13":
-        description = "Heavy Freezing Rain";
+        description = Strings.type_13;
         imagePath = "";
         break;
       case "type_14":
-        description = "Light Freezing Rain";
+        description = Strings.type_14;
         imagePath = "";
         break;
       case "type_15":
-        description = "Funnel Cloud/Tornado";
+        description = Strings.type_15;
         imagePath = "";
         break;
       case "type_16":
-        description = "Hail Showers";
+        description = Strings.type_16;
         imagePath = "";
         break;
       case "type_17":
-        description = "Ice";
+        description = Strings.type_17;
         imagePath = "";
         break;
       case "type_18":
-        description = "Lightning Without Thunder";
+        description = Strings.type_18;
         imagePath = "";
         break;
       case "type_19":
-        description = "Mist";
+        description = Strings.type_19;
         imagePath = "";
         break;
       case "type_20":
-        description = "Precipitation In Vicinity";
+        description = Strings.type_20;
         imagePath = "";
         break;
       case "type_21":
-        description = "Rain";
+        description = Strings.type_21;
         imagePath = "";
         break;
       case "type_22":
-        description = "Heavy Rain And Snow";
+        description = Strings.type_22;
         imagePath = "";
         break;
       case "type_23":
-        description = "Light Rain And Snow";
+        description = Strings.type_23;
         imagePath = "";
         break;
       case "type_24":
-        description = "Rain Showers";
+        description = Strings.type_24;
         imagePath = "";
         break;
       case "type_25":
-        description = "Heavy Rain";
+        description = Strings.type_25;
         imagePath = "";
         break;
       case "type_26":
-        description = "Light Rain";
+        description = Strings.type_26;
         imagePath = "";
         break;
       case "type_27":
-        description = "Sky Coverage Decreasing";
+        description = Strings.type_27;
         imagePath = "";
         break;
       case "type_28":
-        description = "Sky Coverage Increasing";
+        description = Strings.type_28;
         imagePath = "";
         break;
       case "type_29":
-        description = "Sky Unchanged";
+        description = Strings.type_29;
         imagePath = "";
         break;
       case "type_30":
-        description = "Smoke Or Haze";
+        description = Strings.type_30;
         imagePath = "";
         break;
       case "type_31":
-        description = "Snow";
+        description = Strings.type_31;
         imagePath = "";
         break;
       case "type_32":
-        description = "Snow And Rain Showers";
+        description = Strings.type_32;
         imagePath = "";
         break;
       case "type_33":
-        description = "Snow Showers";
+        description = Strings.type_33;
         imagePath = "";
         break;
       case "type_34":
-        description = "Heavy Snow";
+        description = Strings.type_34;
         imagePath = "";
         break;
       case "type_35":
-        description = "Light Snow";
+        description = Strings.type_35;
         imagePath = "";
         break;
       case "type_36":
-        description = "Squalls";
+        description = Strings.type_36;
         imagePath = "";
         break;
       case "type_37":
-        description = "Thunderstorm";
+        description = Strings.type_37;
         imagePath = "";
         break;
       case "type_38":
-        description = "Thunderstorm Without Precipitation";
+        description = Strings.type_38;
         imagePath = "";
         break;
       case "type_39":
-        description = "Diamond Dust";
+        description = Strings.type_39;
         imagePath = "";
         break;
       case "type_40":
-        description = "Hail";
+        description = Strings.type_40;
         imagePath = "";
         break;
       case "type_41":
-        description = "Overcast";
+        description = Strings.type_41;
         imagePath = "";
         break;
       case "type_42":
-        description = "Partially cloudy";
+        description = Strings.type_42;
         imagePath = "";
         break;
       case "type_43":
-        description = "Clear";
+        description = Strings.type_43;
         imagePath = "";
         break;
       default:
-        description = "Weather conditions undefined";
+        description = Strings.typeUndefined;
         imagePath = "";
     }
     return Condition._(type, description, imagePath);
